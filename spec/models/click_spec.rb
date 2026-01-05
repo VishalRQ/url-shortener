a4a952ac811c049c1f8e05975c464d3f12a5e0d7
Vishal <Vishal@rootquotient.com>
2026-01-05 12:13:10 +0530

    updated unit testing

--- spec/models/click_spec.rb
+++ spec/models/click_spec.rb
@@ -5,5 +5,7 @@ RSpec.describe Click, type: :model do
     url = create(:url)
     click = url.click_records.create(ip: "1.2.3.4", user_agent: "TestAgent")
     expect(click.url).to eq(url)
+    # expect(url.click_records).to include(click)
+    expect(url.click_records.count).to eq(1)
   end
 end
